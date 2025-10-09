db437b4ccb3601a56334fe2d00954cde9979e7c0
SandPotNoodles <77805958+ActuallySandPotNoodles@users.noreply.github.com>
2025-10-09 23:02:59 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+echo "Starting Install Of 'sanddice'..."
+echo "Downloading Stuff..."
+mkdir -p /usr/share/sandpotnoodles
+wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sanddice/refs/heads/main/sanddice.png -O /usr/share/sandpotnoodles/sanddice.png
+wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sanddice/refs/heads/main/sanddice.desktop -O /usr/share/applications/sanddice.desktop
+wget https://github.com/ActuallySandPotNoodles/sanddice/releases/download/release/sanddice -O /usr/bin/sanddice
+chmod 775 /usr/bin/sanddice
+echo "Install Complete!"
