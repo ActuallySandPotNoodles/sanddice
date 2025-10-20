9abf224b785cc74188b49cb61384d179d1d7196b
SandPotNoodles <77805958+ActuallySandPotNoodles@users.noreply.github.com>
2025-10-20 19:35:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ fi
 echo "Starting Install Of 'sanddice'..."
 echo "Downloading Stuff..."
 mkdir -p /usr/share/sandpotnoodles
-wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sanddice/refs/heads/main/sanddice.png -O /usr/share/sandpotnoodles/sanddice.png
+wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sanddice/refs/heads/main/sanddice-new.png -O /usr/share/sandpotnoodles/sanddice.png
 wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sanddice/refs/heads/main/sanddice.desktop -O /usr/share/applications/sanddice.desktop
 wget https://github.com/ActuallySandPotNoodles/sanddice/releases/download/v1.0.1/sanddice -O /usr/bin/sanddice
 chmod 775 /usr/bin/sanddice
